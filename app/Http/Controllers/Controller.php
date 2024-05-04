5caf85522e4c531fd6b33ff20ce1d26a7514040c
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-05-04 16:11:12 +0700

    final update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,46 +2,7 @@
 
 namespace App\Http\Controllers;
 
-/**
- * @OA\Info(
- *      version="1.0.0",
- *      title="Todo API",
- *      description="Todo API",
- *      @OA\Contact(
- *          email="khoa@gmail.com"
- *      ),
- *      @OA\License(
- *          name="Apache 2.0",
- *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
- *      )
- * )
- */
-/**
- * @OA\Server(
- *      url=L5_SWAGGER_CONST_HOST,
- *      description="Todo API server"
- * )
- */
-
-/**
- * @OA\SecurityScheme(
- *      securityScheme="bearerAuth",
- *      type="http",
- *      scheme="bearer",
- * )
- */
-
-/**
- * @OA\Get(
- *      path="/",
- *      summary="Show the application welcome screen (default route)",
- *      @OA\Response(
- *          response=200,
- *          description="Successful operation"
- *       )
- * )
- */
-abstract class Controller
+class Controller
 {
-    //
+
 }
--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -7,28 +7,70 @@ use App\Http\Requests\UpdateTodoReqest;
 use App\Http\Resources\TodoCollection;
 use App\Http\Resources\TodoResouce;
 use App\Models\Todo;
-use App\Http\Traits\HttpResponses;
-use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Your API Title",
+ *      description="Your API Description",
+ *      termsOfService="http://example.com/terms/",
+ *      @OA\Contact(
+ *          email="contact@example.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
 
 class TodoController extends Controller
 {
-    use HttpResponses;
-
     /**
      * Display a listing of the todos.
      *
+     * @OA\Get(
+     *     path="/api/todos",
+     *     summary="Get all todos",
+     *     tags={"Todos"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="List of todos",
+     *
+     *     )
+     * )
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
         $todos = Todo::all();
-        return $this->success(new TodoCollection($todos));
+        return new TodoCollection($todos);
     }
 
     /**
      * Store a newly created todo in storage.
      *
+     * @OA\Post(
+     *     path="/api/todos",
+     *     summary="Create a new todo",
+     *     tags={"Todos"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *            @OA\JsonContent(
+     *             required={"title"},
+     *              @OA\Property(property="user_id", type="integer"),
+     *             @OA\Property(property="title", type="string"),
+     *             @OA\Property(property="description", type="string"),
+     *              @OA\Property(property="completed", type="boolean"),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Todo created successfully",
+     *
+     *     )
+     * )
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
@@ -37,12 +79,38 @@ class TodoController extends Controller
         $validatedData = $request->validated();
 
         $todo = Todo::create($validatedData);
-        return $this->success(new TodoResouce($todo), 201);
+        return new TodoResouce($todo, 201);
     }
 
     /**
      * Display the specified todo.
      *
+     * @OA\Get(
+     *     path="/api/todos/{id}",
+     *     summary="Get a specific todo by ID",
+     *     tags={"Todos"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the todo",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *
+     *
+     *     @OA\Response(
+     *         response=200,
+     *         description="Todo details",
+     *
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Todo not found"
+     *     )
+     * )
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
@@ -51,37 +119,93 @@ class TodoController extends Controller
         $todo = Todo::find($id);
 
         if (!$todo) {
-            return $this->error(null, 'Todo not found', 404);
+            return response()->json(['error' => 'Todo not found'], 404);
         }
 
-
-        return $this->success(new TodoResouce($todo));
+        return new TodoResouce($todo);
     }
 
     /**
      * Update the specified todo in storage.
      *
+     * @OA\Put(
+     *     path="/api/todos/{id}",
+     *     summary="Update a specific todo by ID",
+     *     tags={"Todos"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the todo",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *      @OA\RequestBody(
+     *         required=true,
+     *            @OA\JsonContent(
+     *             required={"title"},
+     *              @OA\Property(property="user_id", type="integer"),
+     *             @OA\Property(property="title", type="string"),
+     *             @OA\Property(property="description", type="string"),
+     *              @OA\Property(property="completed", type="boolean"),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Todo updated successfully",
+     *
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Todo not found"
+     *     )
+     * )
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(UpdateTodoReqest $request, $id)
     {
-
-
         $todo = Todo::find($id);
 
         if (!$todo) {
-            return $this->error(null, 'Todo not found', 404);
+            return response()->json(['error' => 'Todo not found'], 404);
         }
+
         $validatedData = $request->validated();
         $todo->update($validatedData);
-        return $this->success(new TodoResouce($todo));
+        return new TodoResouce($todo);
     }
+
 
     /**
      * Remove the specified todo from storage.
      *
+     * @OA\Delete(
+     *     path="/api/todos/{id}",
+     *     summary="Delete a specific todo by ID",
+     *     tags={"Todos"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the todo",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="Todo deleted successfully"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Todo not found"
+     *     )
+     * )
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
@@ -90,9 +214,10 @@ class TodoController extends Controller
         $todo = Todo::find($id);
 
         if (!$todo) {
-            return $this->error(null, 'Todo not found', 404);
+            return response()->json(['error' => 'Todo not found'], 404);
         }
+
         $todo->delete();
-        return $this->success(null, 'Todo deleted');
+        return response()->json(null, 204);
     }
 }
